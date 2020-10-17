3a265ad37914845d1c84864b060f09580bfa7912
moyu <47513906+Ru0ch3n@users.noreply.github.com>
2020-10-17 17:50:35 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+"""
+    Primality Testing by Fermat's Little Theorem
+    基于费马小定理的素性检测
+
+    @author:柴若晨
+    @num:18180200028
+    @class:1818039
+    人生苦短，我用Py
+"""
+
+"""
+    Dependencies
+"""
+from random import *
+from egcd import egcd as gcd
+"""
+    Initialize
+"""
+#target储存目标数
+target = eval(input("请输入target number"))
+if(target%2 == 0):
+    print("输入的不是奇整数，是偶数")
+    exit(2)
+#k存储安全参数k
+k = eval(input("请输入安全参数k\n"))
+#用概率ans定义是素数的概率，设初值为0
+ans = 0
+#a存储[2,m-2]中的一个随机数，先随意设个初值为-1
+a = 0
+
+"""
+    StartTest
+"""
+#判断k回
+for i in range(0,k):
+    #选取一个[2,m-2]的数字，即[2,m-1)。注意randint下是左开右闭区间
+    a = randint(2,target-1)
+    if(gcd(a,target) != 1):
+        print("用",a,"已经判断出是合数")
+        exit(2)
+    if(((a**(target-1))-1)%target != 0):
+        print("用",a,"已经判断出是合数")
+        exit(2)
+    
+print("m是素数的概率是",1-1/2**k)
